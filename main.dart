3adc9c6737d74c3a09caf4c8b413277412cc08fd
durvesh <thes86088@gmail.com>
2026-06-26 14:13:51 +0530

    add skeleton for constructor and build method for ItemCard class

--- main.dart
+++ main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
+class ItemCard extends StatelessWidget {
+  
+  ItemCard( Color labelColor, String itemName, int amount ){
+    super( { super.key } ) ;
+    labelColor: labelColor ;
+    itemName : itemName ;
+    amount = amount ;
+  }
+  
+  @override
+  Widget build( BuildContext context ) {
+    return Row(
+    
+    )
+  }
+  
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
